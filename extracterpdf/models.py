510fdf7d35356b9cb5a60837ac802192368bb398
gianrocon <gianrocon@hotmail.com>
2019-05-09 01:33:13 -0300

    commit do pycharm

--- extracterpdf/models.py
+++ extracterpdf/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import os
 from website.settings import BASE_DIR
 
+pre_path_pdfs = os.path.join(BASE_DIR, "static", "extracterpdf")
+path_pdfs = os.path.join(pre_path_pdfs, "pdfs_extraidos")
 
 class Destinos(models.Model):
-    pdffile = models.FileField(upload_to=os.path.join(BASE_DIR, "static", "extracterpdf" ,"pdfs_extraidos"))
+    pdffile = models.FileField(upload_to=path_pdfs)
