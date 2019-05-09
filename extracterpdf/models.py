2429fc973fbacd1efa96e86bcb7a5134ef345a94
gianrocon <gianrocon@hotmail.com>
2019-05-09 02:04:31 -0300

    commit do pycharm

--- extracterpdf/models.py
+++ extracterpdf/models.py
@@ -7,7 +7,7 @@ from website.settings import BASE_DIR
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     #return 'user_{0}/{1}/{2}/{3}'.format(instance.user.id, filename)
 
-path = os.path.join("hrdb", "static", "extracterpdf" ,"pdfs_extraidos")
+path = os.path.join("micro", "static", "extracterpdf" ,"pdfs_extraidos")
 
 class Destinos(models.Model):
     pdffile = models.FileField(upload_to=path)
